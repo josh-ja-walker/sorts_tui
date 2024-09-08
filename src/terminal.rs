1e5099c130425da614a449c48ec1b76fd06b3d51
Josh Walker <josh.walker1@outlook.com>
2024-09-08 22:42:37 +0100

    Updated starting bar gap to 3

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,12 +1,17 @@
-use std::{io::{stderr, Write}, time::Duration};
+use std::time::Duration;
 
 use ratatui::{
-	crossterm::event::{self, Event, KeyCode, KeyEventKind}, layout::{Constraint, Layout}, style::Style, text::Line, widgets::{Bar, BarChart, BarGroup, Block, Borders, Padding}, DefaultTerminal 
+	crossterm::event::{self, Event, KeyCode, KeyEventKind}, 
+	layout::{Constraint, Layout}, 
+	style::Style, 
+	text::Line, 
+	DefaultTerminal,
+	widgets::{Bar, BarChart, BarGroup, Block, Borders, Padding}, 
 };
 
 use crate::{sort_type::SortType, Error};
 
-const BAR_GAP: u16 = 1;
+const MAX_BAR_GAP: u16 = 4;
 const BAR_WIDTH_MIN: u16 = 1;
 const BAR_WIDTH_MAX: u16 = 3;
 
@@ -37,19 +42,28 @@ fn build_bars(bar_width: u16, data: &Vec<u64>) -> BarGroup {
 }
 
 /* Calculate width and gap of bars */
-fn calc_bar_sizes(term_width: u16, num_items: usize) -> Result<(u16, u16), Error> {
-	let mut bar_gap: u16 = BAR_GAP;
+fn calc_bar_sizes(term_width: u16, num_items: usize) -> (u16, u16) {
+	let usable_term_width = term_width - HORIZ_PAD - CHART_PAD;
+	let mut bar_gap: u16 = MAX_BAR_GAP;
 	
 	loop {
+		/* Width of all gaps */
 		let total_gap_width: u16 = bar_gap * (num_items - 1) as u16;
-		let bar_width: f32 = (term_width - HORIZ_PAD - CHART_PAD - total_gap_width) as f32 / (num_items as f32);
-		
-		if bar_width < 1.0 {
-			bar_gap = bar_gap.checked_sub(1).expect("Bar width could not be calculated");
-			continue;
-		}
+
+		/* If terminal width - total gaps > 0 */
+		if let Some(all_bars_width) = usable_term_width.checked_sub(total_gap_width) {
+			/* Calculate individual bar width */
+			let bar_width: f32 = all_bars_width as f32 / (num_items as f32);
 			
-		return Ok(((bar_width.floor() as u16).clamp(BAR_WIDTH_MIN, BAR_WIDTH_MAX), bar_gap));
+			/* If bar width is valid, clamp and return */
+			if bar_width >= 1.0 {
+				let clamped_bar_width = (bar_width.floor() as u16).clamp(BAR_WIDTH_MIN, BAR_WIDTH_MAX);
+				return (clamped_bar_width, bar_gap);
+			}
+		} 
+		
+		/* If gap is invalid, minus 1 and reattempt */
+		bar_gap = bar_gap.checked_sub(1).expect("Bar width could not be calculated");
 	}
 }
 
@@ -76,23 +90,28 @@ impl Terminal {
     /* Render the bar chart */
 	pub fn render(&mut self, sort: SortType, data: &Vec<u64>) -> Result<(), Error> {
 		self.term.draw(|frame| {
-			let (bar_width, bar_gap) = calc_bar_sizes(frame.area().width, data.len()).unwrap();
+			/* Calculate bar width and gaps */
+			let (bar_width, bar_gap) = calc_bar_sizes(frame.area().width, data.len());
 			
-			/* Chart Width = n * (width + gap) - overlap + padding */
+			/* Chart Width = n * (width + gap) - extra gap + padding */
 			let chart_width = (data.len() as u16 * (bar_width + bar_gap)) - bar_gap + CHART_PAD;
 			
+			/* Set up layout of chart - set width and center */
 			let [_, area, _] = Layout::horizontal([
-				Constraint::Fill(1),
-				Constraint::Length(chart_width + HORIZ_PAD), 
-				Constraint::Fill(1),
-				]).vertical_margin(5)
+					Constraint::Fill(1),
+					Constraint::Length(chart_width + HORIZ_PAD), 
+					Constraint::Fill(1),
+				])
+				.vertical_margin(5) 
 				.areas(frame.area());
 			
+			/* Set up containing block */
 			let block = Block::default()
-				.title(sort.to_string())
+				.title(Line::styled(sort.to_string(), sort.color()))
 				.padding(Padding::new(HORIZ_PAD / 2, HORIZ_PAD / 2, 2, 0))
 				.borders(Borders::ALL);
 
+			/* Set up bar chart */
 			let bar_chart = BarChart::default()
 				.block(block)
 				.bar_style(Style::default().fg(sort.color()))
@@ -100,6 +119,7 @@ impl Terminal {
 				.bar_gap(bar_gap)
 				.data(build_bars(bar_width, data));
 
+			/* Render bar chart with set area */
 			frame.render_widget(bar_chart, area);
 		})?;
 
