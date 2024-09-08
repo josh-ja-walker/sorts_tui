23628ea666c7d1d044e01b7fac0ec06296b75684
Josh Walker <josh.walker1@outlook.com>
2024-09-08 22:23:34 +0100

    Clamped bar width and fixed overflow on high numbers of items

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{io::{stderr, Write}, time::Duration};
 
 use ratatui::{
 	crossterm::event::{self, Event, KeyCode, KeyEventKind}, layout::{Constraint, Layout}, style::Style, text::Line, widgets::{Bar, BarChart, BarGroup, Block, Borders, Padding}, DefaultTerminal 
@@ -7,6 +7,11 @@ use ratatui::{
 use crate::{sort_type::SortType, Error};
 
 const BAR_GAP: u16 = 1;
+const BAR_WIDTH_MIN: u16 = 1;
+const BAR_WIDTH_MAX: u16 = 3;
+
+const HORIZ_PAD: u16 = 4;
+const CHART_PAD: u16 = 2;
 
 
 /* Build group of bars from the data */
@@ -31,6 +36,23 @@ fn build_bars(bar_width: u16, data: &Vec<u64>) -> BarGroup {
 			.collect::<Vec<Bar>>())
 }
 
+/* Calculate width and gap of bars */
+fn calc_bar_sizes(term_width: u16, num_items: usize) -> Result<(u16, u16), Error> {
+	let mut bar_gap: u16 = BAR_GAP;
+	
+	loop {
+		let total_gap_width: u16 = bar_gap * (num_items - 1) as u16;
+		let bar_width: f32 = (term_width - HORIZ_PAD - CHART_PAD - total_gap_width) as f32 / (num_items as f32);
+		
+		if bar_width < 1.0 {
+			bar_gap = bar_gap.checked_sub(1).expect("Bar width could not be calculated");
+			continue;
+		}
+			
+		return Ok(((bar_width.floor() as u16).clamp(BAR_WIDTH_MIN, BAR_WIDTH_MAX), bar_gap));
+	}
+}
+
 
 pub struct Terminal {
 	term: DefaultTerminal
@@ -50,51 +72,30 @@ impl Terminal {
 		ratatui::restore();
 		Ok(())
 	}
-
-	/* Calculate width and gap of bars */
-	fn calc_bar_sizes(term_width: u16, num_items: usize) -> Result<(u16, u16), Error> {
-		let mut bar_gap: u16 = BAR_GAP;
-		
-		loop {
-			let bar_width = (term_width - (bar_gap * (num_items) as u16)) / num_items as u16;
-			
-			if bar_width < 1 {
-				bar_gap = bar_gap.checked_sub(1).expect("Bar width could not be calculated");
-			} else {
-				return Ok((bar_width, bar_gap));
-			}
-		}
-	}
-
+	
     /* Render the bar chart */
 	pub fn render(&mut self, sort: SortType, data: &Vec<u64>) -> Result<(), Error> {
 		self.term.draw(|frame| {
-			let horiz_pad: u16 = 4;
-
-			let (bar_width, bar_gap) = Self::calc_bar_sizes(
-				frame.area().width - horiz_pad, 
-				data.len()
-			).unwrap();
+			let (bar_width, bar_gap) = calc_bar_sizes(frame.area().width, data.len()).unwrap();
 			
-			/* Chart Width = n * (width + gap) + trailing gap */
-			let chart_width = ((bar_width + bar_gap) * data.len() as u16) + bar_gap;
+			/* Chart Width = n * (width + gap) - overlap + padding */
+			let chart_width = (data.len() as u16 * (bar_width + bar_gap)) - bar_gap + CHART_PAD;
 			
 			let [_, area, _] = Layout::horizontal([
-					Constraint::Min(0), 
-					Constraint::Max(chart_width + horiz_pad), 
-					Constraint::Min(0)
+				Constraint::Fill(1),
+				Constraint::Length(chart_width + HORIZ_PAD), 
+				Constraint::Fill(1),
 				]).vertical_margin(5)
 				.areas(frame.area());
 			
 			let block = Block::default()
 				.title(sort.to_string())
-				.padding(Padding::new(horiz_pad / 2, horiz_pad / 2, 2, 0))
+				.padding(Padding::new(HORIZ_PAD / 2, HORIZ_PAD / 2, 2, 0))
 				.borders(Borders::ALL);
 
 			let bar_chart = BarChart::default()
 				.block(block)
 				.bar_style(Style::default().fg(sort.color()))
-				.value_style(Style::default())
 				.bar_width(bar_width)
 				.bar_gap(bar_gap)
 				.data(build_bars(bar_width, data));
